32e0e412c3d1392672a196b3330dca0363e2426a
Jesper Asplund <jesper.asplund95@gmail.com>
2019-11-12 15:45:01 +0100

    testing out darkmode toggle with react hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import StartPage from './views/StartPage'
 
-function App() {
+const App = () => {
+  const [darkmode, setDarkmode] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={darkmode ? 'App dark-mode' : 'App'}>
+      <StartPage />
+      <button onClick={()=>setDarkmode(!darkmode)}>TOGGLE MODE</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/views/StartPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const StartPage = () => {
+  return(
+    <div className="startpage">
+      <h1>This is the startpage!</h1>
+      <p>This is some bread crumbs....</p>
+    </div>
+  )
+}
+
+export default StartPage
