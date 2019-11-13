7b17cc470bfb37654d5205471c8a07a7e36342ed
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-11-13 11:39:25 +0100

    Included icons in menu and styling adjustments

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,28 +1,66 @@
 import React, { useState } from 'react'
+import { Row, Col, CustomInput } from 'reactstrap'
+import { Home, Users, Eye, User, Plus, Heart, Settings, HelpCircle, LogOut } from 'react-feather'
 
 const Menu = () => {
   return (
-    <nav>
+    <nav className="menu">
       <ul>
-        <li>Lasse Skida</li>
-        <li>Startsida</li>        
-        <li>Mina barn</li>
-        <ul>
-          <li>Översikt</li>
-          <li>Larry Skida</li>
-          <li>Ragnar Skida</li>
-          <li>Lägg till barn</li>
-        </ul>
-        <li>Favoriter</li>        
-        <li>Mitt konto</li>        
-        <li>Frågor och svar</li>              
+        <li><h4><u>Lasse Skida</u></h4></li>
+        <li>
+          <Home />
+          <span>Startsida</span>
+        </li>
+        <li>
+          <Users />
+          <span>Mina barn</span>
+          <ul>
+            <li>
+              <Eye />
+              <span>Översikt</span>
+            </li>
+            <li>
+              <User />
+              <span>Larry Skida</span>
+            </li>
+            <li>
+              <User />
+              <span>Ragnar Skida</span>
+            </li>
+            <li>
+              <Plus />
+              <span>Lägg till barn</span>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <Heart />
+          <span>Favoriter</span>
+        </li>
+        <li>
+          <Settings />
+          <span>Mitt konto</span>
+        </li>
+        <li>
+          <HelpCircle />
+          <span>Frågor och svar</span>
+        </li>
+        <li className="dark-mode-box">
+          <Row className="no-gutters align-items-center">
+            <Col>
+              <p>Mörkt telefonläge</p>
+              <p>För ökad mysfaktor i tillvaron</p>
+            </Col>
+            <Col xs="auto">
+              <CustomInput type="switch" id="dark-mode" name="dark-mode" />
+            </Col>
+          </Row>
+        </li>
+        <li>
+          <LogOut />
+          <span>Logga ut</span>
+        </li>
       </ul>
-      <div>
-        Mörkt telefonläge
-      </div>
-      <div>
-        Logga ut
-      </div>
     </nav>
   )
 }
