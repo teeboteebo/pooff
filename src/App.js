eefe786877ed7e8dd2913ae84c2114a9f8310081
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-11-13 22:09:51 +0100

    Auto stash before revert of "Created header component and imported it to App"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import Menu from './components/Menu'
+import Header from './components/Header'
 // import StartPage from './views/StartPage'
 
 const App = () => {
   const [darkmode, setDarkmode] = useState(false)
   return (
     <div className={darkmode ? 'App dark-mode' : 'App'}>
-      <Menu />
+      <Header />
+      <main>
+      </main>
     </div>
   )
 }
