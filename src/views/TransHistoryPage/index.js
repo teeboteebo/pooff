6027430fdd059d9323d29c1fa932227302a06ced
edling <joel.edling1@gmail.com>
2019-11-13 20:39:28 +0100

    View file added

---
+++ src/views/TransHistoryPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Tabs from '../../components/TransactionHistory'
+
+const TransHistoryPage = () => {
+  return (
+    <div className="trans-history">
+      <h2>Transaktioner</h2>
+      <Tabs />
+    </div>
+  )
+}
+
+export default TransHistoryPage
