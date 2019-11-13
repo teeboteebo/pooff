2597f0f2531cca1163f22f5736b0ad967fa17fa1
Kevin Carvajal <kevin.carvajal.malmo@gmail.com>
2019-11-13 19:43:24 +0100

    Started to create Login Component
    somehow reactstrap doesnt work..?

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
-import StartPage from './views/StartPage'
+import React, { useState } from "react";
+import StartPage from "./views/StartPage";
+import LoginPage from "./views/LoginPage";
 
 const App = () => {
-  const [darkmode, setDarkmode] = useState(false)
+  const [darkmode, setDarkmode] = useState(false);
   return (
-    <div className={darkmode ? 'App dark-mode' : 'App'}>
+    <div className={darkmode ? "App dark-mode" : "App"}>
       <StartPage />
-      <button onClick={()=>setDarkmode(!darkmode)}>TOGGLE MODE</button>
+      <button onClick={() => setDarkmode(!darkmode)}>TOGGLE MODE</button>
+      <LoginPage />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Login/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+const Login = () => {
+  return (
+    <Container fluid>
+      <Row>
+        <Col xs="3">hello</Col>
+        <Col xs="9">hello</Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Login;
