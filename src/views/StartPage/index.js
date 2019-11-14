0ff0cb52857d06c013b5ea68534113a6f0af88e9
Jesper Asplund <jesper.asplund95@gmail.com>
2019-11-14 22:55:11 +0100

    Buttons now on startpage with a dropdown for "TopUp"

--- src/views/StartPage/index.js
+++ src/views/StartPage/index.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container, Row, Col } from 'reactstrap'
+import { Plus, DollarSign } from 'react-feather'
+
+import TopUp from '../../components/TopUp'
 import TransactionHistoryPreview from '../../components/TransactionHistoryPreview'
 
 const StartPage = () => {
+  
   let integer, decimal
   const getBalanceAndSplit = async () => {
     let balance = 522000.69 // axios get mytrans/balance
@@ -10,16 +15,28 @@ const StartPage = () => {
     decimal = balance[1]
   }
   getBalanceAndSplit()
+  const [topUpToggle, setTopUpToggle] = useState(false)
   return (
-    <div className="startpage">
+    <Container className="startpage">
       <div className="page-content">
         <h2 className="page-title">
           <span className="amount-integer">{integer}</span>
           <span className="amount-decimal">,{decimal} kr</span>
-        </h2>        
+        </h2>
+        <Row className="buttons">
+          <Col xs={{ size: "5", offset: 1 }}>
+            <button className="pay-btn"><DollarSign size="34" /></button>
+            <p>Betala</p>
+          </Col>
+          <Col xs="5">
+            <button onClick={() => setTopUpToggle(true)}className="top-up-btn"><Plus size="34" /></button>
+            <p>Fyll på pengar</p>
+          </Col>
+        </Row>
       </div>
       <TransactionHistoryPreview />
-    </div>
+      <TopUp toggle={topUpToggle} clickHandler={() => setTopUpToggle(false)}/>
+    </Container>
   )
 }
 
