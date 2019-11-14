6145a88fdace1d6438a307422b583b0b9d31ce87
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-11-14 11:35:53 +0100

    Included react-router-dom in menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
 import { Row, Col, CustomInput } from 'reactstrap'
 import { Home, Users, ChevronDown, Eye, User, Plus, Heart, Settings, HelpCircle, LogOut } from 'react-feather'
 
@@ -6,65 +7,75 @@ const Menu = () => {
   // const [foldedMenu, setFoldedMenu] = useState(true)
 
   return (
-    <nav className='menu open'>
-      <ul>
-        <li><h4><u>Lasse Skida</u></h4></li>
-        <li>
-          <Home />
-          <span>Startsida</span>
-        </li>
-        <li>
-          <Users />
-          <span>Mina barn</span>
-          <ChevronDown />
-          <ul>
-            <li>
-              <Eye />
-              <span>Översikt</span>
-            </li>
-            <li>
-              <User />
-              <span>Larry Skida</span>
-            </li>
-            <li>
-              <User />
-              <span>Ragnar Skida</span>
-            </li>
-            <li>
-              <Plus />
-              <span>Lägg till barn</span>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <Heart />
-          <span>Favoriter</span>
-        </li>
-        <li>
-          <Settings />
-          <span>Mitt konto</span>
-        </li>
-        <li>
-          <HelpCircle />
-          <span>Frågor och svar</span>
-        </li>
-        <li className="dark-mode-box">
-          <Row className="no-gutters align-items-center">
-            <Col>
-              <p>Mörkt telefonläge</p>
-              <p className="cozy-text">För ökad mysfaktor i tillvaron</p>
-            </Col>
-            <Col xs="auto">
-              <CustomInput type="switch" id="dark-mode" name="dark-mode" />
-            </Col>
-          </Row>
-        </li>
-        <li>
-          <LogOut />
-          <span>Logga ut</span>
-        </li>
-      </ul>
-    </nav>
+    <Router>
+      <nav className='menu open'>
+        <ul>
+          <li><h5><u>Lasse Skida</u></h5></li>
+          <li>
+            <Link to="/">
+              <Home />
+              <span>Startsida</span>
+            </Link>
+          </li>
+          <li>
+            <Users />
+            <span>Mina barn</span>
+            <ChevronDown />
+            <ul>
+              <li>
+                <Eye />
+                <span>Översikt</span>
+              </li>
+              <li>
+                <User />
+                <span>Larry Skida</span>
+              </li>
+              <li>
+                <User />
+                <span>Ragnar Skida</span>
+              </li>
+              <li>
+                <Plus />
+                <span>Lägg till barn</span>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <Link>
+              <Heart />
+              <span>Favoriter</span>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <Settings />
+              <span>Mitt konto</span>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <HelpCircle />
+              <span>Frågor och svar</span>
+            </Link>
+          </li>
+          <li className="dark-mode-box">
+            <Row className="no-gutters align-items-center">
+              <Col>
+                <p>Mörkt telefonläge</p>
+                <p className="cozy-text">För ökad mysfaktor i tillvaron</p>
+              </Col>
+              <Col xs="auto">
+                <CustomInput type="switch" id="dark-mode" name="dark-mode" />
+              </Col>
+            </Row>
+          </li>
+          <li>
+            <LogOut />
+            <span>Logga ut</span>
+          </li>
+        </ul>
+      </nav>
+    </Router>
   )
 }
 
