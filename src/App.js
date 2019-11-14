d1b943b1c5cba7b1573408e9bb76f5ef6beebe49
Jesper Asplund <jesper.asplund95@gmail.com>
2019-11-14 22:54:17 +0100

    Added fix for properly displaying 100vh height on a mobile browser

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import StartPage from './views/StartPage'
 import TransHistoryPage from './views/TransHistoryPage'
 const App = () => {
+  let vh = window.innerHeight * 0.01;  
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
   const [darkmode, setDarkmode] = useState(false)
   document.querySelector('body').addEventListener("keyup", (e)=> {
     if(e.keyCode === 192) setDarkmode(!darkmode)
