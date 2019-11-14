21745c00a9b50302e15a71fd671929688aea670d
Oliver <oliver.nilsson94@gmail.com>
2019-11-14 12:06:59 +0100

    added front end for sending reset mail

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
-import StartPage from './views/StartPage'
+import React, { useState } from "react"
+import ResetPassword from "./views/ResetPassword"
 
 const App = () => {
   const [darkmode, setDarkmode] = useState(false)
   return (
-    <div className={darkmode ? 'App dark-mode' : 'App'}>
-      <StartPage />
-      <button onClick={()=>setDarkmode(!darkmode)}>TOGGLE MODE</button>
+    <div className={darkmode ? "App dark-mode" : "App"}>
+      <ResetPassword />
     </div>
   )
 }
