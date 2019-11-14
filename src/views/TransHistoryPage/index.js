596eb64fc66697b2013d2115d73468ae9547e329
edling <joel.edling1@gmail.com>
2019-11-14 09:05:15 +0100

    center Transactions

--- src/views/TransHistoryPage/index.js
+++ src/views/TransHistoryPage/index.js
@@ -4,7 +4,7 @@ import Tabs from '../../components/TransactionHistory'
 const TransHistoryPage = () => {
   return (
     <div className="trans-history">
-      <h2>Transaktioner</h2>
+      <h2 className="text-center">Transaktioner</h2>
       <Tabs />
     </div>
   )
