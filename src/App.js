f9414d5de8bbf7f0412e3120e47847381678d4a9
Jesper Asplund <jesper.asplund95@gmail.com>
2019-11-14 12:00:04 +0100

    toggle for darkmode on "§"

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import StartPage from './views/StartPage'
 
 const App = () => {
   const [darkmode, setDarkmode] = useState(false)
+  document.querySelector('body').addEventListener("keyup", (e)=> {
+    if(e.keyCode === 192) setDarkmode(!darkmode)
+  })
   return (
     <div className={darkmode ? 'App dark-mode' : 'App'}>
       {/* <Header /> */}
