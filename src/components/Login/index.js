e177213e3835461a39f815d15dfe113990d1f5ad
Kevin Carvajal <kevin.carvajal.malmo@gmail.com>
2019-11-15 15:08:42 +0100

    styling/validation
    only did a little of validation just to understand how i can use hooks

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,13 +1,48 @@
-import React from "react";
-import { Container, Row, Col } from "reactstrap";
+import React, {useState } from "react";
+import { Form, Row, Col, Input, Button, Label } from "reactstrap";
 const Login = () => {
-  return (
-    <Container fluid>
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    alert(
+      `Email: ${name} \n Password: ${password}`
+    
+    )
+}
+return (
+    <Form onSubmit={onSubmit}>
       <Row>
-        <Col xs="3">hello</Col>
-        <Col xs="9">hello</Col>
+        <Col sm="12" md={{ size: 6, offset: 3 }}>
+        <Label  className="floating-label" for="exampleEmail">Email</Label>
+          <Input
+          type="email"
+          value={name}
+          onChange={e => setName(e.target.value)}
+          className="form-control"
+          placeholder="yourname@example.com" 
+          ></Input>
+        </Col>
+        <Col sm="12" md={{ size: 6, offset: 3 }}>
+        <Label className="floating-label" for="examplePassword">Lösenord</Label>
+
+          <Input 
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          className="form-control" 
+          placeholder="Lösenord"
+         ></Input>
+        </Col>
       </Row>
-    </Container>
+
+      <a href="/">glömt lösenord?</a>
+      <Row>
+      <Button type="submit" value="Submit" >Logga in</Button>
+      </Row>
+    </Form>
   );
 };
 
