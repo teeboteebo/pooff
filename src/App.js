497dbde9b6f8aeb076d29b07e784d0c34fdf6398
Fredrik Sjolin <sjolin.fredrik@gmail.com>
2019-11-15 10:24:59 +0100

    darkmode toggle for windows §

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import CreateNewUserPage from './views/CreateNewUserPage'
 const App = () => {
   const [darkmode, setDarkmode] = useState(false)
   document.querySelector('body').addEventListener("keyup", (e) => {
-    if (e.keyCode === 192) setDarkmode(!darkmode)
+    console.log(e.keyCode)
+    if (e.keyCode === 192 || e.keyCode === 220) setDarkmode(!darkmode)
   })
   return (
     <Router>
