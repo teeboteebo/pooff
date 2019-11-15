c208a7eb20a2a497efdf7f922cb8bbcd80f5c2df
edling <joel.edling1@gmail.com>
2019-11-15 15:19:12 +0100

    styling content, button added

--- src/views/TransactionPage/index.js
+++ src/views/TransactionPage/index.js
@@ -1,143 +1,84 @@
 import React from 'react';
-import { Col, Row, Container } from 'reactstrap';
+import { ArrowLeft } from 'react-feather'
+import { Link } from 'react-router-dom'
+import { Col, Row, Container, Button } from 'reactstrap';
 
 const TransactionPage = () => {
 
   // const transactions = await fetch('/api/mytransactions')
-  // const transactions = [
-  //   {
-  //     sender: {
-  //       firstName: 'Don',
-  //       lastName: 'Bank'
-  //     },
-  //     receiver: {
-  //       firstName: 'Lasse',
-  //       lastName: 'Skida'
-  //     },
-  //     amount: 10000,
-  //     message: "Top up",
-  //     date: Date.now(),
-  //   },
-  //   {
-  //     sender: {
-  //       firstName: 'Don',
-  //       lastName: 'Bank'
-  //     },
-  //     receiver: {
-  //       firstName: 'Lasse',
-  //       lastName: 'Skida'
-  //     },
-  //     amount: 10000,
-  //     message: "Top up",
-  //     date: Date.now(),
-  //   },
-  //   {
-  //     sender: {
-  //       firstName: 'Don',
-  //       lastName: 'Bank'
-  //     },
-  //     receiver: {
-  //       firstName: 'Lasse',
-  //       lastName: 'Skida'
-  //     },
-  //     amount: 10000,
-  //     message: "Top up",
-  //     date: Date.now(),
-  //   },
-  //   {
-  //     sender: {
-  //       firstName: 'Don',
-  //       lastName: 'Bank'
-  //     },
-  //     receiver: {
-  //       firstName: 'Lasse',
-  //       lastName: 'Skida'
-  //     },
-  //     amount: 10000,
-  //     message: "Top up",
-  //     date: Date.now(),
-  //   },
-  //   {
-  //     sender: {
-  //       firstName: 'Don',
-  //       lastName: 'Bank'
-  //     },
-  //     receiver: {
-  //       firstName: 'Lasse',
-  //       lastName: 'Skida'
-  //     },
-  //     amount: 100000,
-  //     message: "Top up",
-  //     date: Date.now(),
-  //   },
-  //   {
-  //     sender: {
-  //       firstName: 'Don',
-  //       lastName: 'Bank'
-  //     },
-  //     receiver: {
-  //       firstName: 'Lasse',
-  //       lastName: 'Skida'
-  //     },
-  //     amount: 10000,
-  //     message: "Top up",
-  //     date: Date.now(),
-  //   },
-  //   {
-  //     sender: {
-  //       firstName: 'Lasse',
-  //       lastName: 'Bank'
-  //     },
-  //     receiver: {
-  //       firstName: 'Bank',
-  //       lastName: 'Skida'
-  //     },
-  //     amount: -10000,
-  //     message: "Top up",
-  //     date: Date.now(),
-  //   },
-  //   {
-  //     sender: {
-  //       firstName: 'Don',
-  //       lastName: 'Bank'
-  //     },
-  //     receiver: {
-  //       firstName: 'Lasse',
-  //       lastName: 'Skida'
-  //     },
-  //     amount: 10000,
-  //     message: "Top up",
-  //     date: Date.now(),
-  //   }
-  // ]
+  const transactions = [
+    {
+      sender: {
+        firstName: 'Don',
+        lastName: 'Bank',
+        phone: '97884 83884',
+      },
+      receiver: {
+        firstName: 'Lasse',
+        lastName: 'Skida',
+        phone: '0700029929',
+      },
+      amount: 10000,
+      message: "Mega festligt med sär skrivning och sånt",
+      date: Date.now(),
+    },
+  ]
 
   // let allSent = []
   // let allReceived = []
 
-  // transactions.forEach(trans => {
-  //   if (trans.amount > 0) {
-  //     allReceived.push(trans)
-  //   } else if (trans.amount < 0) {
-  //     allSent.push(trans)
-  //   }
-  // })
-  return (
-    <Container className="transaction" fluid={true}>
-      <h2 className="page-title">Transaktion</h2>
-      <Row>
-        <Col xs="12">
-          <div className="card">
-            <div className="card-body mx-auto">
-              <p className="name"> Fullname </p>
-              <p className="phone">Phone</p>
-              <p className="amount">Amount</p>
-              <p className="message">Message</p>
+  //  let val = transactions.find((trans, i) => {
+  //    return 
+  //  })
+  //   return (
+  // <Container className="transaction" fluid={true}>
+  //   <h2 className="page-title">Transaktion</h2>
+  //   <Row>
+  //     <Col xs="12">
+  //       <div className="card">
+  //         <div className="card-body mx-auto">
+  //           <p className="name"> {val.firstName} </p>
+  //           <p className="phone">Phone</p>
+  //           <p className="amount">Amount</p>
+  //           <p className="message">"Message"</p>
+  //         </div>
+  //       </div>
+  //     </Col>
+  //   </Row>
+  // </Container>
+  //   )
+
+  let transaction = transactions.map((trans, i) => {
+    // format date and remove punctuation
+    // let date = new Date(transaction.date).toLocaleString('sv-SE', { day: "numeric", month: "short" })
+    // date = date.substring(0, date.length - 1);
+    return (
+      <Container className="transaction" key={'trans' + i} fluid={true}>
+        <h2 className="page-title">Transaktion</h2>
+        <Row>
+          <Col xs="12">
+            <div className="card">
+              <div className="card-body mx-auto d-flex flex-column">
+                <p className="name"> {trans.receiver.firstName + ' ' + trans.receiver.lastName} </p>
+                <p className="phone">{trans.receiver.phone}</p>
+                <p className="amount"> {trans.amount} </p>
+                <p className="message">" {trans.message} "</p>
+
+              </div>
             </div>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  )
+          </Col>
+        </Row>
+        {/* <div className="mt-auto"> */}
+          <Link to="/mina-transaktioner">
+            <Button className="back-btn"><ArrowLeft /></Button>
+          </Link>
+        {/* </div> */}
+      </Container>
+    )
+
+  })
+  return transaction
+
 }
 
 export default TransactionPage
