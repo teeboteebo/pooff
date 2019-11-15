a9f09d61e8369bba0010dc553ce260c80d45275a
Oliver <oliver.nilsson94@gmail.com>
2019-11-15 14:40:34 +0100

    can now change password using email, only need backend enc. for new pass

--- src/views/NewPassword/index.js
+++ src/views/NewPassword/index.js
@@ -2,38 +2,60 @@ import React, { useState, useEffect } from "react"
 
 const NewPassword = () => {
   const [link, setLink] = useState("")
+  const [updated, setUpdated] = useState(false)
 
   useEffect(() => {
     const path = window.location.pathname.split("/")[2]
     const fetchLink = async () => {
       let fetchedLink = await fetch("/api/links/" + path)
       fetchedLink = await fetchedLink.json()
-      fetchedLink.link ? await setLink(fetchedLink) : await setLink("")
+      fetchedLink.link ? setLink(fetchedLink) : setLink("")
     }
     fetchLink()
   }, [])
 
-  const findUserAndChangePassword = async () => {}
+  const findUserAndChangePassword = async () => {
+    const user = await findUser()
+    await updatePassword(user)
+    setUpdated(true)
+  }
 
   const findUser = async () => {
-    fetch("/api/users/id/:id")
+    let user = await fetch("/api/users/email/" + link.email)
+    user = user.json()
+    return user
+  }
+
+  const updatePassword = async user => {
+    await fetch(`/api/users/id/${user[0]._id}/edit`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        password: document.querySelector(".new-password").value,
+      }), // We send data in JSON format
+    })
   }
 
   return (
     <div>
       {link ? (
-        <div className="new-password-container">
-          <h1>Välj nytt lösenord</h1>{" "}
-          <label className="new-password-item">Nytt lösenord</label>
-          <input className="new-password-item"></input>
-          <label className="new-password-item">Upprepa lösenord</label>
-          <input className="new-password-item"></input>
-          <button onClick={findUserAndChangePassword}>Bekräfta</button>
-        </div>
+        link && !updated ? (
+          <div className="new-password-container">
+            <h1>Välj nytt lösenord</h1>{" "}
+            <label className="new-password-item">Nytt lösenord</label>
+            <input className="new-password"></input>
+            <label className="new-password-item">Upprepa lösenord</label>
+            <input className="new-password-confirm"></input>
+            <button onClick={findUserAndChangePassword}>Bekräfta</button>
+          </div>
+        ) : (
+          <h1>Lösenord uppdaterat</h1>
+        )
       ) : (
         <p>Link not found</p>
       )}
-      {console.log(link)}
     </div>
   )
 }
