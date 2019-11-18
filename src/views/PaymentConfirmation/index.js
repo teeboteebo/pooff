f636ba15fc0958739da84a80d34e462acdf7a33a
Oliver <oliver.nilsson94@gmail.com>
2019-11-18 11:00:32 +0100

    responsiveness for payment confirm

--- src/views/PaymentConfirmation/index.js
+++ src/views/PaymentConfirmation/index.js
@@ -1,21 +1,24 @@
 import React from "react"
 import "./styles.scss"
+import { Link } from "react-router-dom"
 
 const PaymentConfirmation = props => {
   return (
     <div className="payment-confirmation-container">
-      <h1 className="title-field">Betalning skickad</h1>
+      <h3 className="title-field">Betalning skickad</h3>
       <img
         className="check-mark"
         src="/images/checkmarkLM.png"
         alt="checkmark"
       ></img>
-      <h2 className="name-field">{props.name}</h2>
-      <h3 className="number-field">{props.number}</h3>
-      <h2 className="amount-field">{props.amount}</h2>
-      <h3 className="message-field">{props.message}</h3>
-      <button>Till transaktionshistorik</button>
-      <button>Till startsida</button>
+      <h4 className="name-field">{props.name}</h4>
+      <h6 className="number-field">{props.number}</h6>
+      <h4 className="amount-field">{props.amount}</h4>
+      <h6 className="message-field">{props.message}</h6>
+      <Link className="link-button">Till transaktionshistorik</Link>
+      <Link to="/" className="link-button">
+        Till startsida
+      </Link>
     </div>
   )
 }
