65822e1ab1e7a909481992cc5a57e1eebf6afa4a
edling <joel.edling1@gmail.com>
2019-11-18 11:12:25 +0100

    little stylin done

--- src/views/TransactionPage/index.js
+++ src/views/TransactionPage/index.js
@@ -24,58 +24,33 @@ const TransactionPage = () => {
     },
   ]
 
-  // let allSent = []
-  // let allReceived = []
-
-  //  let val = transactions.find((trans, i) => {
-  //    return 
-  //  })
-  //   return (
-  // <Container className="transaction" fluid={true}>
-  //   <h2 className="page-title">Transaktion</h2>
-  //   <Row>
-  //     <Col xs="12">
-  //       <div className="card">
-  //         <div className="card-body mx-auto">
-  //           <p className="name"> {val.firstName} </p>
-  //           <p className="phone">Phone</p>
-  //           <p className="amount">Amount</p>
-  //           <p className="message">"Message"</p>
-  //         </div>
-  //       </div>
-  //     </Col>
-  //   </Row>
-  // </Container>
-  //   )
-
   let transaction = transactions.map((trans, i) => {
     // format date and remove punctuation
-    // let date = new Date(transaction.date).toLocaleString('sv-SE', { day: "numeric", month: "short" })
-    // date = date.substring(0, date.length - 1);
+    let date = new Date(trans.date).toLocaleString('sv-SE', { day: "numeric", month: "short" })
+    date = date.substring(0, date.length - 1);
     return (
       <Container className="transaction" key={'trans' + i} fluid={true}>
         <h2 className="page-title">Transaktion</h2>
         <Row className="no-gutters">
           <Col xs="12">
             <div className="card">
-              <div className="card-body mx-auto d-flex flex-column">
+              <div className="card-body ">
                 <p className="name"> {trans.receiver.firstName + ' ' + trans.receiver.lastName} </p>
                 <p className="phone">{trans.receiver.phone}</p>
-                <p className="amount"> {trans.amount} </p>
+                <p className="amount"> {trans.amount}kr </p>
                 <p className="message">" {trans.message} "</p>
-
+                <p className="message">" {date} "</p>
               </div>
             </div>
           </Col>
         </Row>
-        {/* <div className="mt-auto"> */}
+        <div className="mt-3">
           <Link to="/mina-transaktioner">
             <Button className="back-btn"><ArrowLeft /></Button>
           </Link>
-        {/* </div> */}
+        </div>
       </Container>
     )
-
   })
   return transaction
 
