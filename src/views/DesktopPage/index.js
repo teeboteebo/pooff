364e4aaf6d1d250c325b464b7984cf0b983d48d8
Jesper Asplund <jesper.asplund95@gmail.com>
2019-11-19 21:53:12 +0100

    first version of the desktop landing page

--- src/views/DesktopPage/index.js
+++ src/views/DesktopPage/index.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import {Container } from 'reactstrap'
+import { Container, Row, Col } from 'reactstrap'
 
 const DesktopPage = () => {
   return (
-    <Container fluid={true}>
-      <p>GO MOBILE</p>
+    <Container fluid={true} className="desktop-container">
+      <Row className="h-100 no-gutters">
+        <Col md="5" className="img-side">
+          <img src="/images/phones.jpg" alt="images of ihpones displaying pooff app" />
+        </Col>
+        <Col md="1" className="triangle" />
+        <Col md="6" className="text-side">
+          <div className="text-content">
+            <h2 className="page-title">Welcome to Pooff</h2>
+            <p>Pooff is an online payment service available for all mobile and handheld devices.</p>
+            <p>Please continue on your phone in order to access the application.</p>
+            <p>Or press here to head over to our frequently asked questions section.</p>
+            <p>/ Pooff team</p>
+          </div>
+          <img className="logo" src="/images/logos/pooff-dark.png" alt="pooff logo" />
+        </Col>
+      </Row>
 
     </Container>
   )
