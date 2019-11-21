5026ad94790f0e67fce1696a6fe947bb87eb4335
Oliver <oliver.nilsson94@gmail.com>
2019-11-21 15:15:50 +0100

    added recaptcha

--- src/views/StartPage/index.js
+++ src/views/StartPage/index.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react'
-import { Container, Row, Col } from 'reactstrap'
-import { Plus, DollarSign } from 'react-feather'
+import React, { useState } from "react"
+import { Container, Row, Col } from "reactstrap"
+import { Plus, DollarSign } from "react-feather"
+import ReCAPTCHA from "react-google-recaptcha"
 
-import TopUp from '../../components/TopUp'
-import TransactionHistoryPreview from '../../components/TransactionHistoryPreview'
+import TopUp from "../../components/TopUp"
+import TransactionHistoryPreview from "../../components/TransactionHistoryPreview"
 
 const StartPage = () => {
-  
   let integer, decimal
   const getBalanceAndSplit = async () => {
     let balance = 522000.69 // axios get mytrans/balance
-    balance = balance.toLocaleString('sv-SE').split(',')
+    balance = balance.toLocaleString("sv-SE").split(",")
     integer = balance[0]
     decimal = balance[1]
   }
@@ -25,17 +25,22 @@ const StartPage = () => {
         </h2>
         <Row className="buttons">
           <Col xs={{ size: "5", offset: 1 }}>
-            <button className="pay-btn"><DollarSign size="22" /></button>
+            <button className="pay-btn">
+              <DollarSign size="22" />
+            </button>
             <p>Betala</p>
           </Col>
           <Col xs="5">
-            <button onClick={() => setTopUpToggle(true)}className="top-up-btn"><Plus size="22" /></button>
+            <button onClick={() => setTopUpToggle(true)} className="top-up-btn">
+              <Plus size="22" />
+            </button>
             <p>Fyll på pengar</p>
           </Col>
         </Row>
       </div>
       <TransactionHistoryPreview />
-      <TopUp toggle={topUpToggle} clickHandler={() => setTopUpToggle(false)}/>
+      <TopUp toggle={topUpToggle} clickHandler={() => setTopUpToggle(false)} />
+      <ReCAPTCHA sitekey="6Ld28MMUAAAAAPKfYvE9JUgE8kuUGn1OwFAtHrQZ" />
     </Container>
   )
 }
