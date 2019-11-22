58a7a2e4e5a30adffc558ef53dbbd56263d1dbf1
edling <joel.edling1@gmail.com>
2019-11-22 15:12:02 +0100

    removed the routes

--- src/App.js
+++ src/App.js
@@ -99,8 +99,6 @@ const App = () => {
                 <Route exact path="/" render={(props) => <LoginPage {...props} loginHandler={checkIfLoggedIn} />} />
                 <Route exact path="/registrera" component={CreateNewUserPage} />
                 <Route exact path="/login-test" render={(props) => <LoginPage {...props} loginHandler={checkIfLoggedIn} />} />
-                <Route exact path="/mina-transaktioner" component={TransHistoryPage} />
-                <Route exact path="/enskild-transaktion/:id" component={TransactionPage} />
                 <Route exact path="/registrera-barn" component={createUserAsChild} />
                 <Route exact path="/vanliga-fragor" component={QnA} />
               </Switch>
