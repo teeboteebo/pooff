6e70a31b17eb202c9868da4a6cc8d0f46824f508
Fredrik Sjolin <sjolin.fredrik@gmail.com>
2019-11-22 15:19:03 +0100

    started on poofStartPage

---
+++ src/components/PooffHeader/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PooffHeader = () => {
+
+  return (
+    <Container fluid={true}>
+      <div>
+        <header className="pooff-header">POOFF</header>
+      </div>
+    </Container>
+  )
+}
+export default PooffHeader
