239c68350d3d4fa3575528847f2aa07c239f66c4
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-11-23 14:05:45 +0100

    App listen after context loggedIn and login updates the context state

--- src/context.js
+++ src/context.js
@@ -1,10 +1,16 @@
 import { createContext, useContext, useState } from 'react'
 
 export const useContextValues = () => {
+  const [loggedIn, setLoggedIn] = useState()
+  const [children, setChildren] = useState()
   const [darkMode, setDarkMode] = useState(false)
   const [menuOpen, setMenuOpen] = useState(false)
 
   return {
+    loggedIn,
+    setLoggedIn,
+    children,
+    setChildren,
     darkMode,
     setDarkMode,
     menuOpen,
