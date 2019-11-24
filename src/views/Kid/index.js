d59fc7d34e8b14a09bba85dfc55c644ea43c501d
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-11-24 16:43:33 +0100

    Kid view now gets data from context

--- src/views/Kid/index.js
+++ src/views/Kid/index.js
@@ -1,58 +1,38 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Container, Spinner } from 'reactstrap'
 import { Plus } from 'react-feather'
 
+import { usePooff } from '../../context'
+
 import TransactionLister from '../../components/TransactionLister'
 
 const Kid = () => {
-  const [child, setChild] = useState()
+  const state = usePooff()
+
   const [filter, setFilter] = useState('all')
+
   const { id } = useParams()
 
-  useEffect(() => {
-    const getChild = async () => {
-      const response = await fetch('/api/mychildren')
-      const fetchedChildren = await response.json()
-      const child = fetchedChildren.find(child => child._id === id)
-      child.filteredTransactions = child.transactions
-      setChild(child)
-    }
+  if (state.children.length) {
+    const child = state.children.find(child => child._id === id)
 
-    getChild()
-  }, [])
-
-  const filterTransactions = (filter) => {
-    const { transactions } = child
-    if (filter === 'received') {
-      setChild({
-        ...child,
-        filteredTransactions: transactions.filter(transaction => transaction.amount > 0)
-      })
-      setFilter('received')
-      return
-    }
-    if (filter === 'sent') {
-      setChild({
-        ...child,
-        filteredTransactions: transactions.filter(transaction => transaction.amount < 0)
-      })
-      setFilter('sent')
-      return
-    }
-    setChild({
-      ...child,
-      filteredTransactions: transactions
-    })
-    setFilter('all')
-    return
-  }
-
-  if (child) {
-    let { firstName, lastName, balance, filteredTransactions } = child
+    let { firstName, lastName, balance, transactions } = child
 
     balance = (balance.toFixed(2) + '').split('.')
-    
+
+    let filteredTransactions
+    switch (filter) {
+      case 'received':
+        filteredTransactions = transactions.filter(transaction => transaction.amount > 0)
+        break
+      case 'sent':
+        filteredTransactions = transactions.filter(transaction => transaction.amount < 0)
+        break
+      default:
+        filteredTransactions = transactions
+    }
+
     return (
       <Container className="kid">
         <h2 className="page-title">{firstName + ' ' + lastName}</h2>
@@ -65,15 +45,15 @@ const Kid = () => {
           <p>För över pengar</p>
         </div>
         <div className="sort-section mb-4">
-          <button onClick={() => filterTransactions('all')} className={filter === 'all' ? 'left active' : 'left'}>Alla</button>
-          <button onClick={() => filterTransactions('received')} className={filter === 'received' ? 'middle active' : 'middle'}>Mottagna</button>
-          <button onClick={() => filterTransactions('sent')} className={filter === 'sent' ? 'right active' : 'right'}>Skickade</button>
+          <button onClick={() => setFilter('all')} className={filter === 'all' ? 'left active' : 'left'}>Alla</button>
+          <button onClick={() => setFilter('received')} className={filter === 'received' ? 'middle active' : 'middle'}>Mottagna</button>
+          <button onClick={() => setFilter('sent')} className={filter === 'sent' ? 'right active' : 'right'}>Skickade</button>
         </div>
         <TransactionLister transactions={filteredTransactions} />
       </Container>
     )
   }
-
+  
   return (
     <Container className="kid">
       <Spinner />
