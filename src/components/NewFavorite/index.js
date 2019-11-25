8b19a92cace37aec364fbae57d2f350b00cac452
Jesper Asplund <jesper.asplund95@gmail.com>
2019-11-25 16:51:31 +0100

    Connected favorites to db - add, get all & delete

--- src/components/NewFavorite/index.js
+++ src/components/NewFavorite/index.js
@@ -1,5 +1,5 @@
-import React, {useState } from "react";
-import { Form, Row, Col, Input, Button, Label, Container, ModalHeader,ModalBody,ModalFooter, Modal } from "reactstrap";
+import React, { useState } from "react";
+import { Form, Row, Col, Input, Button, Label, Container, ModalHeader, ModalBody, ModalFooter, Modal } from "reactstrap";
 
 
 
@@ -8,64 +8,73 @@ const NewFavorite = (props) => {
   //skapa values för namn och telefonnr när en användare skapas
   const [nameFavorite, setNameFavorite] = useState("");
   const [phoneFavorite, setPhoneFavorite] = useState("");
-
-  const sendFavorite = (evt) => {
-    evt.preventDefault();
-    console.log({nameFavorite}, {phoneFavorite})
-  }
-
-  const {
-    buttonLabel,
-    className
-  } = props;
- 
- 
   const [modal, setModal] = useState(false);
- 
+
+  const sendFavorite = async (evt) => {
+    evt.preventDefault();
+    console.log({ nameFavorite }, { phoneFavorite })
+    const responseRaw = await fetch('/api/myuser/favorites', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        nickname: nameFavorite,
+        phone: phoneFavorite
+      })
+    })
+    let message = await responseRaw.json()
+    console.log('fetch sent');
+    
+    console.log(message);
+    setModal(false)
+    props.updateFavorites()
+  }
+  
+
+
   const toggle = () => setModal(!modal);
 
-return (
-  <Container>
-     <Button onClick={toggle}>{buttonLabel}Ny Favorit</Button>
-     <Modal isOpen={modal} toggle={toggle} className={className}>
-       <ModalHeader toggle={toggle}>
-         Skapa Favorit
+  return (
+    <Container>
+      <Button className="primary-btn" onClick={toggle}>Ny Favorit</Button>
+      <Modal isOpen={modal} toggle={toggle} className="add-favorite-container">
+        <ModalHeader toggle={toggle}>
+          Skapa Favorit
          </ModalHeader>
-       <ModalBody>
-       <Form onSubmit={sendFavorite}>
-     <Row className="input-field">
-       <Col sm="12" md={{ size: 6, offset: 3 }}>
-       <Label  className="floating-label" for="exampleEmail">Användarnamn</Label>
-         <Input
-         type="text"
-         value={nameFavorite}
-         onChange={e => setNameFavorite(e.target.value)}
-         className="form-control"
-         placeholder="Namn" 
-         ></Input>
-       </Col>
-       <Col sm="12" md={{ size: 6, offset: 3 }}>
-       <Label className="floating-label" for="examplePassword">Lösenord</Label>
-         <Input 
-         type="text" 
-         pattern="[0-9]*"
-         value={phoneFavorite}
-         onChange={e => setPhoneFavorite(e.target.value)}
-         className="form-control" 
-         placeholder="Telefonnr"
-        ></Input>
-       </Col>
-     </Row>
+        <ModalBody>
+          <Form onSubmit={sendFavorite}>
+            <Row className="input-field">
+              <Col sm="12" md={{ size: 6, offset: 3 }}>
+                <Label className="floating-label" for="exampleEmail">Användarnamn</Label>
+                <Input
+                  type="text"
+                  value={nameFavorite}
+                  onChange={e => setNameFavorite(e.target.value)}
+                  className="form-control"
+                  placeholder="Namn"
+                ></Input>
+              </Col>
+              <Col sm="12" md={{ size: 6, offset: 3 }}>
+                <Label className="floating-label" for="examplePassword">Lösenord</Label>
+                <Input
+                  type="text"
+                  pattern="[0-9]*"
+                  value={phoneFavorite}
+                  onChange={e => setPhoneFavorite(e.target.value)}
+                  className="form-control"
+                  placeholder="Telefonnr"
+                ></Input>
+              </Col>
+            </Row>
 
-   </Form>        
-   </ModalBody>
-       <ModalFooter className="text-center">
-       <Col className="text-center" sm="12" md={{ size: 6, offset: 3 }}>
-       <Button color="primary" onClick={sendFavorite}>Skapa Favorit</Button>
-       </Col>
-       </ModalFooter>
-     </Modal>
-   </Container>
+          </Form>
+          <Button className="primary-btn mt-4" onClick={sendFavorite}>Spara Favorit</Button>
+
+        </ModalBody>
+        
+      </Modal>
+    </Container>
   );
 };
 
--- src/views/FavoritePage/index.js
+++ src/views/FavoritePage/index.js
@@ -1,36 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Button } from "reactstrap";
-import {ChevronRight} from 'react-feather';
+import { X } from 'react-feather';
 import NewFavorite from "../../components/NewFavorite";
-    
 
 
-const FavoritePage = ()  =>{
-        return (
-           <Container>
-               <Row>
-                   <Col sm="12"  md={{ size: 6, offset: 3 }}>
-                       <h1 className="text-center">Favoriter</h1>
-                    </Col>
-               </Row>
-               <Row>
-               <Col className="text-center" sm="12"  md={{ size: 6, offset: 3 }}>
-                   <Button className="Favorite-user">
-                   <h3>Larry skida</h3>
-                   <p>+0707 0707 0707</p>
-                   <ChevronRight/>
-                   </Button>
-               </Col>
-               </Row>
-               
 
-               <Row className="button-field">
-                    <Col className="text-center" sm="12" md={{ size: 6, offset: 3 }}>
-                    <NewFavorite/>
-                    </Col>
-                </Row>  
-           </Container>
-        );
+const FavoritePage = () => {
+  const [userFavorites, setUserFavorites] = useState('')
+
+  const removeFavorite = async (phone) => {
+    let favoritesAfterDelete = await fetch(`/api/myuser/favorites/${phone}`, { method: 'DELETE' })
+    let newFavorites = await favoritesAfterDelete.json()
+    setUserFavorites(newFavorites)
+  }
+  const getAllUserFavorites = async () => {
+    const allFavoritesRaw = await fetch('/api/myuser/favorites')
+    const allFavorites = await allFavoritesRaw.json()
+    if (allFavorites && allFavorites !== userFavorites) setUserFavorites(allFavorites)
+  }
+  useEffect(() => {
+    getAllUserFavorites()
+  }, [])
+
+  return (
+    <Container>
+
+      <h2 className="page-title">Favoriter</h2>
+      <Row className="no-gutters">
+        {userFavorites ? userFavorites.map((favorite, i) => {
+          return (
+            <Col xs="12" className="mb-3 bg-primary" key={"favorite_" + i}>
+              <p>{favorite.nickname}</p>
+              <p>{favorite.phone}</p>
+              <Button className="remove-btn" onClick={() => removeFavorite(favorite.phone)}>Ta bort <X /></Button>
+            </Col>
+          )
+        }) : ''}
+      </Row>
+
+
+      <Row className="button-field">
+        <Col className="text-center" sm="12" md={{ size: 6, offset: 3 }}>
+          <NewFavorite updateFavorites={getAllUserFavorites} />
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default FavoritePage;
