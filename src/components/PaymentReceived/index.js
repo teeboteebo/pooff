3a82ffc3f8b4d016624676b0f1787d71506dcab0
Oliver <oliver.nilsson94@gmail.com>
2019-12-03 16:03:58 +0100

    link to transaction

--- src/components/PaymentReceived/index.js
+++ src/components/PaymentReceived/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import useMagic from '../../actions/useMagic'
 
 const PaymentReceived = props => {
+  const [getLoggedIn] = useMagic()
   console.log("props", props.data) 
-  let { sender, amount, message } = props.data
+  let { sender, amount, message, link } = props.data
+  getLoggedIn()
   return (
-    <div className="payment-received-container"> {sender} skickade {amount}kr </div>
+    <Link to={`/mina-transaktioner/${link}`}>
+      <div className="payment-received-container"> {sender} skickade {amount}kr </div>
+      </Link>
   )
 } 
 
