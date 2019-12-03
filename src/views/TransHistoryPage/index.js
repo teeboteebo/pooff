a6c37c34383124ca9d2adb4f90d5c854a7c7394b
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-12-03 10:22:38 +0100

    My transactions filter is the same as child transactions filter

--- src/views/TransHistoryPage/index.js
+++ src/views/TransHistoryPage/index.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Nav, NavItem, NavLink, Container } from 'reactstrap';
-import classnames from 'classnames';
+import React, { useState } from 'react';
+import { Container } from 'reactstrap';
 
 import { usePooff } from '../../context'
 
@@ -8,72 +7,32 @@ import TransactionLister from '../../components/TransactionLister'
 
 const TransHistoryPage = () => {
   const state = usePooff()
-  const [activeTab, setActiveTab] = useState('1');
 
-  const [transactions, setTransactions] = useState([]);
-  const [receivedTransaction, setReceivedTransactions] = useState([]);
-  const [sentTransaction, setSentTransactions] = useState([]);
+  const [filter, setFilter] = useState('all')
 
-  const toggle = tab => {
-    if (activeTab !== tab) setActiveTab(tab);
+  const { transactions } = state.loggedIn
+
+  let filteredTransactions
+  switch (filter) {
+    case 'received':
+      filteredTransactions = transactions.filter(transaction => transaction.amount > 0)
+      break
+    case 'sent':
+      filteredTransactions = transactions.filter(transaction => transaction.amount < 0)
+      break
+    default:
+      filteredTransactions = transactions
   }
-
-  useEffect(() => {
-    function getTransaction() {
-      let allSent = []
-      let allReceived = []
-      /* const transactionsRaw = await fetch('/api/mytransactions')
-      const transactions = await transactionsRaw.json() */
-      const { transactions } = state.loggedIn
-      transactions.forEach(trans => {
-        if (trans.amount > 0) {
-          allReceived.push(trans)
-        } else if (trans.amount < 0) {
-          allSent.push(trans)
-        }
-      })
-      setTransactions(transactions)
-      setReceivedTransactions(allReceived);
-      setSentTransactions(allSent)
-    }
-    getTransaction();
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
 
   return (
     <Container className="trans-history">
       <h2 className="page-title">Transaktioner</h2>
-      <Nav tabs className="tab-select">
-        <NavItem className="items">
-          <NavLink
-            className={classnames('nav-link', { active: activeTab === '1' })}
-            onClick={() => { toggle('1'); }}
-          >
-            Alla
-          </NavLink>
-        </NavItem>
-        <NavItem className="items">
-          <NavLink
-            className={classnames('nav-link', { active: activeTab === '2' })}
-            onClick={() => { toggle('2'); }}
-          >
-            Mottagna
-          </NavLink>
-        </NavItem>
-        <NavItem className="items">
-          <NavLink
-            className={classnames('nav-link', { active: activeTab === '3' })}
-            onClick={() => { toggle('3'); }}
-          >
-            Skickade
-          </NavLink>
-        </NavItem>
-      </Nav>
-      { activeTab === '1' ? <TransactionLister transactions={transactions} /> : null}
-      { activeTab === '2' ? <TransactionLister transactions={receivedTransaction} /> : null}
-      { activeTab === '3' ? <TransactionLister transactions={sentTransaction} /> : null}
+      <div className="sort-section mb-4">
+        <button onClick={() => setFilter('all')} className={filter === 'all' ? 'left active' : 'left'}>Alla</button>
+        <button onClick={() => setFilter('received')} className={filter === 'received' ? 'middle active' : 'middle'}>Mottagna</button>
+        <button onClick={() => setFilter('sent')} className={filter === 'sent' ? 'right active' : 'right'}>Skickade</button>
+      </div>
+      <TransactionLister transactions={filteredTransactions} />
     </Container>
   )
 }
