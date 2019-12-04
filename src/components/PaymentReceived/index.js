31a241381684899d3fa114ab13b447c147944779
Oliver <oliver.nilsson94@gmail.com>
2019-12-04 12:41:38 +0100

    styling

--- src/components/PaymentReceived/index.js
+++ src/components/PaymentReceived/index.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import useMagic from '../../actions/useMagic'
+import Fade from 'react-reveal/Fade'
 
 const PaymentReceived = props => {
   const [getLoggedIn] = useMagic()
   console.log("props", props.data) 
   let { sender, amount, message, link } = props.data
 
-  setTimeout(props.clickHandler, 5000)
+  //setTimeout(props.clickHandler, 5000)
 
   getLoggedIn()
   return (
-    <Link to={`/mina-transaktioner/${link}`}>
-      <div className="payment-received-container" onClick={props.clickHandler}> {sender} skickade {amount}kr </div>
+    <div className="payment-received-container">
+    <Fade className="payment-fade" right collapse opposite duration={1000}>
+      <Link to={`/mina-transaktioner/${link}`}>
+        <div className="payment-received-card" onClick={props.clickHandler}> {sender} skickade {amount}kr </div>
       </Link>
+      </Fade>
+      </div>
   )
 } 
 
