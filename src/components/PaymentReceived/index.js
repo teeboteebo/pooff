a4fbed8f7431c2337382457e9edfa80fc2f2eb8d
Oliver <oliver.nilsson94@gmail.com>
2019-12-06 11:37:28 +0100

    done

--- src/components/PaymentReceived/index.js
+++ src/components/PaymentReceived/index.js
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import useMagic from '../../actions/useMagic'
 import Fade from 'react-reveal/Fade'
 
 const PaymentReceived = props => {
   const [getLoggedIn] = useMagic()
-  console.log("props", props.data) 
-  let { sender, amount, message, link } = props.data
+  let { sender, amount, link } = props.data
+  const [show, setShow] = useState(false)
 
-  //setTimeout(props.clickHandler, 5000)
-
+  useEffect(() => {
+    setShow(true)
+  },[])
+  
+  const setFade = () => {
+    setShow(false)
+    setTimeout(props.clickHandler, 1000)
+  }
+  
+  setTimeout(setFade, 5000)
+  
   getLoggedIn()
   return (
     <div className="payment-received-container">
-    <Fade className="payment-fade" right collapse opposite duration={1000}>
+      <Fade className="payment-fade" right collapse opposite when={show}>
       <Link to={`/mina-transaktioner/${link}`}>
         <div className="payment-received-card" onClick={props.clickHandler}> {sender} skickade {amount}kr </div>
       </Link>
